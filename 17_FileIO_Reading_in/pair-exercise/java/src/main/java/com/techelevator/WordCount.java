74d302306459e955ee06b2b24f4402b81722d4d7
Marte Shepherd <blootaz@rocketmail.com>
2021-01-04 15:02:57 -0500

    pair exercise  - incomplete

--- 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
+++ 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
@@ -2,13 +2,44 @@ package com.techelevator;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class WordCount {
 
-	public static void main(String[] args) {
+	public static void readAliceAdventuresInWonderland() 
+	{
 
+		String filePath = "alices_adventures_in_wonderland.txt";
+		File alicesFile = new File(filePath);
+		
+		Scanner scanner;
+		try
+		{
+			scanner = new Scanner(alicesFile.getAbsoluteFile());
+			int counter = 0;
+			while(scanner.hasNextLine()) // when should we stop looping? when there are no more lines
+			{
+				
+				
+				String line = scanner.nextLine();
+				String[] parts = line.split(" ");
+				for (String words: parts)
+				{
+					counter++;
+				}
+				
+				System.out.println(counter);
+			}
+				
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("File: " + filePath + " was not found.");
+		}
+		
 	}
 
 }

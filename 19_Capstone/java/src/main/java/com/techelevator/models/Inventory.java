d6f6be39d97094e78e917f02551b61a1620e6d7a
Jacob Griswold <jacobmgriswold@gmail.com>
2021-01-08 09:51:23 -0500

    yep

--- 19_Capstone/java/src/main/java/com/techelevator/models/Inventory.java
+++ 19_Capstone/java/src/main/java/com/techelevator/models/Inventory.java
@@ -16,12 +16,12 @@ public class Inventory
 	private void loadInventory()
 	{
 		FileProductLoader loader = new FileProductLoader();
-<<<<<<< HEAD
+
 
 		
-=======
+
 		items = loader.loadInventory(); //not sure if this is correct
->>>>>>> efc100e484e3947875875c5eb41ea3caf6ae2cc3
+
 	}
 	
 	private Map<Item, Integer> getInventory()
